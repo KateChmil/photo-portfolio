03a4303e51bb1b93e80de0415b8afb38cb24751c
KateChmil <chmilkatecat@gmail.com>
2025-02-05 21:08:55 +0200

    update main js

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,4 @@
-//document.addEventListener("DOMContentLoaded", function (){//
-    //const animImages = document.querySelectorAll(".anim-img");//
 
-    //const observer = new IntersectionObserver((entries) => {
-      //  entries.forEach((entry) => {
-       //     if (entry.isIntersecting) {
-        //        entry.target.classList.add("anim-show");
-        //    }
-        //});
-  //  }, { threshold: 0.5 }); // Trigger when 50% of the image is visible
-
-   // animImages.forEach((img) => {
-    //    img.classList.add("anim-hidden"); // Start hidden
-   //     observer.observe(img);
-   // });
-//});
 document.addEventListener("DOMContentLoaded", function () {
     const animImages = document.querySelectorAll(".anim-img");
 
