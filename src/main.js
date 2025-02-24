652f1f6c11291cb1887fdecee09d9ad7e297e5f0
KateChmil <chmilkatecat@gmail.com>
2025-02-24 21:17:52 +0200

    fixed animation
    in js changed select anim-img to anim-hidden and added classes to images in html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const animImages = document.querySelectorAll(".anim-img");
+    const animImages = document.querySelectorAll(".anim-hidden");
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
