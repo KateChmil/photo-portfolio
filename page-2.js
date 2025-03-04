8b42b69bbbcd6ec5198762414ff0ebfe07ffb305
KateChmil <chmilkatecat@gmail.com>
2025-03-04 12:30:48 +0000

    Deploying to gh-pages from @ KateChmil/photo-portfolio@e47da62fcce556fdd0763b9a52905ee6d00f8cd6 🚀

--- page-2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                                */import{S as e}from"./assets/vendor-CgTBfC_f.js";document.querySelector(".gallery");new e(".gallery a");
-//# sourceMappingURL=page-2.js.map
